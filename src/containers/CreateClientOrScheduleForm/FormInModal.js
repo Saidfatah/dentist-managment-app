066e8025603f39420e164a865a4836c5054d7b3d
SaidFatah <said_designer@outlook.com>
2022-03-22 11:30:43 +0100

    add logs

--- src/containers/CreateClientOrScheduleForm/FormInModal.js
+++ src/containers/CreateClientOrScheduleForm/FormInModal.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "../../components/Modal";
 import CreateClientForm from "./createClientForm";
 import { MODAL_WIDTH } from "../../utils/constants";
 import { STEP_0_HEIGHT } from "./constants";
 
 const FormInModal = () => {
-  const [height, setHeight] = useState(STEP_0_HEIGHT);
-
+  const [height, setHeight] = useState(0);
+  console.log("FormInMOdal")
+  useEffect(()=>{
+    setHeight(STEP_0_HEIGHT)
+  },[])
   return (
     <Modal
       id="ADD_APPOINTMENT_MODAL"
